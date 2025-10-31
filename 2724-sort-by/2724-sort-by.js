0dfaaba3f0b25e5fb86ca1f2f3aebc9dc778e1a4
Diya Fathima <thasnee8090@gmail.com>
2025-10-31 16:16:32 +0530

    Time: 108 ms (75.97%), Space: 71.5 MB (68.2%) - LeetHub

---
+++ 2724-sort-by/2724-sort-by.js
@@ -0,0 +1,9 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+      return arr.sort((a, b) => fn(a) - fn(b));
+};
+console.log(sortBy([5, 4, 1, 2, 3], x => x));
