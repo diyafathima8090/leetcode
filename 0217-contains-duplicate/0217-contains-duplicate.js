dc1e379b8d021be6bb73832f5f9377c91499c001
Diya Fathima <thasnee8090@gmail.com>
2026-07-27 15:18:57 +0530

    Time: 28 ms (32.7%), Space: 79.3 MB (35.67%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const uniqueNumbers = new Set(nums);
+
+    return uniqueNumbers.size !== nums.length;
+};
